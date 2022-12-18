0811de7522d869b98484bf85e8df0180b1fd47be
Sergiu <s.mateiu1717@gmail.com>
2022-12-19 00:01:42 +0200

    Make Task Parceble

--- app/src/main/java/com/example/todoapp/model/Task.kt
+++ app/src/main/java/com/example/todoapp/model/Task.kt
@@ -1,10 +1,38 @@
 package com.example.todoapp.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 class Task(
-    var taskTitle: String,
-    var priority: String,
-    var date: String,
-    var category: String,
+    var taskTitle: String?,
+    var priority: String?,
+    var date: String?,
+    var category: String?,
 //    var completed: Boolean,
 //    var ID: UUID
-)
+):Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+    }
+
+    companion object CREATOR : Parcelable.Creator<Task> {
+        override fun createFromParcel(parcel: Parcel): Task {
+            return Task(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Task?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
